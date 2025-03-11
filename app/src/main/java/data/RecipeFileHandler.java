fca4da758b2c52494de8630bc492e0a82ebfa07d
NanaseTakahashi <Takahashi.Nanase@technopro.com>
2025-03-11 11:32:13 +0900

    解答途中

--- app/src/main/java/data/RecipeFileHandler.java
+++ app/src/main/java/data/RecipeFileHandler.java
@@ -1,7 +1,12 @@
 package data;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RecipeFileHandler {
     private String filePath;
@@ -13,6 +18,10 @@ public class RecipeFileHandler {
     public RecipeFileHandler(String filePath) {
         this.filePath = filePath;
     }
+    // [後で消す]
+    public String getFileHandler() {
+        return this.filePath;
+    }// [ここまで]
 
     /**
      * 設問1: 一覧表示機能
@@ -22,11 +31,11 @@ public class RecipeFileHandler {
      * @return レシピデータ
      */
     public ArrayList<String> readRecipes() {
-        // try {
+        //try {
 
-        // } catch (IOException e) {
-        //     System.out.println("Error reading file:" + e.getMessage());
-        // }
+        //} catch (IOException e) {
+            //System.out.println("Error reading file:" + e.getMessage());
+        //}
         return null;
     }
 
@@ -40,7 +49,21 @@ public class RecipeFileHandler {
      */
      // 
     public void addRecipe(String recipeName, String ingredients) {
+        // [途中] ファイル名 RecipeFileHandlerを利用していない
+        String filename = this.filePath;
+        // [途中] 書き込む内容（ユーザから入力させる）
+        String contentToWrite = "testtesttest";
+        // [途中] 既存のレシピに追加で書き込む
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) {
+            System.out.println(contentToWrite);
+            //writer.write(contentToWrite);
+            //writer.newLine(); // 書き込み後に改行
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         // try {
+        
 
         // } catch (IOException e) {
 
--- app/src/main/java/ui/RecipeUI.java
+++ app/src/main/java/ui/RecipeUI.java
@@ -1,6 +1,10 @@
 package ui;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -37,9 +41,14 @@ public class RecipeUI {
                 switch (choice) {
                     case "1":
                         // 設問1: 一覧表示機能
+                        System.out.println("Recipes:");
+                        displayRecipes(); // [設問1] レシピを表示させる
                         break;
                     case "2":
                         // 設問2: 新規登録機能
+                        //System.out.println(fileHandler.getFileHandler());
+                        addNewRecipe();
+
                         break;
                     case "3":
                         // 設問3: 検索機能
@@ -62,6 +71,21 @@ public class RecipeUI {
      * RecipeFileHandlerから読み込んだレシピデータを整形してコンソールに表示します。
      */
     private void displayRecipes() {
+        ArrayList<String> recipeName = new ArrayList<>();
+        ArrayList<String> ingredients = new ArrayList<>();
+        recipeName.add("Tomato Soup");
+        ingredients.add("Tomatoes, Onion, Garlic, Vegetable Stock");
+
+        if (recipeName != null || ingredients != null) {
+            // レシピデータの表示
+            System.out.println("-----------------------------------");
+            System.out.println("Recipe Name: " + recipeName.get(0));
+            System.out.println("Main Ingredients: " + recipeName.get(0));
+            System.out.println("-----------------------------------");
+        } //else {
+            // 読み込んだレシピデータが空の場合に、メッセージを出力
+            //System.out.println("No recipes available.");
+        //}
 
     }
 
@@ -72,6 +96,21 @@ public class RecipeUI {
      * @throws java.io.IOException 入出力が受け付けられない
      */
     private void addNewRecipe() throws IOException {
+        // [途中] ファイル名 RecipeFileHandlerを利用していない
+        String filename = fileHandler.getFileHandler();
+        // [途中] 書き込む内容（ユーザから入力させる）
+        String contentToWrite = "testtesttest";
+        // [途中] 既存のレシピに追加で書き込む
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) {
+            System.out.println(contentToWrite);
+            //writer.write(contentToWrite);
+            //writer.newLine(); // 書き込み後に改行
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String recipeName = "";
+        String ingredients = "";
+        //addRecipe(recipeName, ingredients);
 
     }
 
